ee90675f1f65f3bd5fd8e736a7448874668f7ef8
msty <0235reimu@gmail.com>
2017-04-19 14:52:05 +0900

    プロジェクトの追加

---
+++ Environ/GameEngine/Header/Math/Arithmetic/Float3.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+/**
+@file CoreSystem.hpp
+@date Create for 2017/03/03 23:26
+
+@author msty
+
+* @brief アプリケーションを管理しているクラス。
+アプリケーション単位で処理する際に使用。
+*/
+
+#include<memory>
+
+namespace Environ {
+
+	/*
+		Float型の値3つの汎用クラス
+	*/
+	class Float3 {
+	public:
+		Float3();
+		~Float3();
+		Float3& Add(const float aX, const float aY, const float aZ);
+
+	private:
+		class __Float3;
+		std::unique_ptr<__Float3>$;
+
+	public:
+		float& x;
+		float& y;
+		float& z;
+
+	};
+
+
+}
--- Environ/__TestProjects/mstybird/SystemTest/Source/main.cc
+++ Environ/__TestProjects/mstybird/SystemTest/Source/main.cc
@@ -2,14 +2,20 @@
 #include<System/Window.hpp>
 #include<Frame\Frame.hpp>
 #include <System/EntryPoint.hpp>
-
+#include<Math\Arithmetic\Float3.hpp>
 
 #include <string>
 
 bool Environ::SetupWizard() {
 
 	int id= Environ::Window::Create("Sample", 100, 100, 300, 300);
+	Float3 f;
+	f.Add(3, 4, 5);
+	float q = 555;
+	float& fa = q;
 
+	f.x = fa;
+	f.Add(5, 0, 0);
 	Environ::Frame::Create(id, 10, 20, 100, 100, "Frame");
 	
 	//Float f;
