4eac0fa2af31a54c1a6d3168fcb1ef349805f84e
msty <0235reimu@gmail.com>
2017-04-19 14:50:36 +0900

    プリコンパイルズミヘッダの設定

---
+++ Environ/Win10_Math/Header/PreCompiled/Integration.hh
@@ -0,0 +1,14 @@
+// stdafx.h : 標準のシステム インクルード ファイルのインクルード ファイル、または
+// 参照回数が多く、かつあまり変更されない、プロジェクト専用のインクルード ファイル
+// を記述します。
+//
+
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Windows ヘッダーから使用されていない部分を除外します。
+
+#include<Configure\Integration.hh>
+#include<d3d11.h>
+#include<SimpleMath.h>
+
+// TODO: プログラムに必要な追加ヘッダーをここで参照してください
