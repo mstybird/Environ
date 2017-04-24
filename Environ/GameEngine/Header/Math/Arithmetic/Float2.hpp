bb56c2194a678483380b24d79d105ded18ca43f8
msty <0235reimu@gmail.com>
2017-04-24 13:50:20 +0900

    Float2クラスの定義

---
+++ Environ/GameEngine/Header/Math/Arithmetic/Float2.hpp
@@ -0,0 +1,70 @@
+#pragma once
+
+/**
+@file CoreSystem.hpp
+@date Create for 2017/03/03 23:26
+
+@author msty
+
+* @brief アプリケーションを管理しているクラス。
+アプリケーション単位で処理する際に使用。
+*/
+
+#include<memory>
+
+namespace Environ {
+
+	/*
+		Float型の値3つの汎用クラス
+	*/
+
+	class Float2 {
+	public:
+		Float2();
+		Float2(float x);
+		Float2(float aX, float aY, float aZ);
+		Float2(const Float2& aV);
+		virtual ~Float2();
+
+		bool operator==(const Float2& aValue)const;
+		bool operator!=(const Float2& aValue)const;
+
+		Float2& operator=(const Float2&aValue);
+
+		Float2& operator+=(const Float2& aValue);
+		Float2& operator+=(float aValue);
+
+		Float2& operator-=(const Float2& aValue);
+		Float2& operator-=(float aValue);
+		
+		Float2& operator*=(const Float2& aValue);
+		Float2& operator*=(float aValue);
+		
+		Float2& operator/=(const Float2& aValue);
+		Float2& operator/=(float aValue);
+
+		Float2 operator+(Float2 aValue)const;
+		Float2 operator+(float aValue)const;
+
+		Float2 operator-(Float2 aValue)const;
+		Float2 operator-(float aValue)const;
+
+		Float2 operator*(Float2 aValue)const;
+		Float2 operator*(float aValue)const;
+
+		Float2 operator/(Float2 aValue)const;
+		Float2 operator/(float aValue)const;
+
+
+	private:
+		class __Float2;
+		std::unique_ptr<__Float2>$;
+
+	public:
+		float& x;
+		float& y;
+
+	};
+
+
+}
